ad42bebb4128f85980639b9aa3ca3c6715e219fb
Rodion <piperov2004@mail.ru>
2023-11-14 16:34:21 +0300

    KS-1-2 solution

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package lesson1
+
+fun main(){
+
+    var count_of_orders: Int = 75
+    val gratitude_order: String = "Благодарим за покупку!"
+
+    println("Кол-во заказов: $count_of_orders")
+    println(gratitude_order)
+
+    var numbersOfWorkers: Int = 2000
+
+//  println("Кол-во работников: $numbersOfWorkers")
+
+    numbersOfWorkers = 1999
+
+    println("Кол-во работников: $numbersOfWorkers")
+
+}
