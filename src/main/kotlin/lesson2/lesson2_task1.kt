77688842986fef4f9245466d6d0a062f146d254b
Rodion <piperov2004@mail.ru>
2023-11-15 18:28:59 +0300

    KS-2-1 solution

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson2
+
+
+fun main(){
+
+    val countOfStudents: Float = 4f
+    val sumOfGrades: Float = 3f + 4f + 3f + 5f
+
+    val average: Float = (sumOfGrades / countOfStudents)
+
+    println("%.2f".format(average))
+
+}
