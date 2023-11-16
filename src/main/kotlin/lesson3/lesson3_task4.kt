4f689fbea21aa49d7e55d663b6a7543f9ad875ec
Rodion <piperov2004@mail.ru>
2023-11-16 20:05:57 +0300

    KS-3-4 solution

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+fun main() {
+
+    var startStep = "E2"
+    var finishStep = "E4"
+    var numberOfStep = 1
+
+    println("$startStep-$finishStep; $numberOfStep")
+
+    startStep = "D2"
+    finishStep = "D3"
+    numberOfStep = 2
+
+    println("$startStep-$finishStep; $numberOfStep")
+
+}
