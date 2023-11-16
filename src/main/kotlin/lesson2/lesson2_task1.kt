52252f4dc15505b6dc82d18c83c175425369ad7b
Rodion <piperov2004@mail.ru>
2023-11-16 18:56:26 +0300

    KS-2-1 solution corrected2

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -6,7 +6,7 @@ fun main() {
     val countOfStudents = 4
     val sumOfGrades: Float = 3f + 4f + 3f + 5f
 
-    val average: Float = (sumOfGrades / countOfStudents).toFloat()
+    val average = sumOfGrades / countOfStudents
 
     println("%.2f".format(average))
 
