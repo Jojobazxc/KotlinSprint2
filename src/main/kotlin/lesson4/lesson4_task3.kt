8499f8d0627a506599c47efdc2beb97b7933cba9
Rodion <piperov2004@mail.ru>
2023-11-18 13:23:04 +0300

    KS-4-3 solution corrected

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -2,11 +2,19 @@ package lesson4
 
 fun main() {
 
-    val weatherIsSunny: Boolean = true
-    val tentIsOpen: Boolean = true
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
     val humidity = 20
     val season = "зима"
 
-    println("Благоприятные ли условия для роста бобовых? ${weatherIsSunny && tentIsOpen && (humidity == 20) && (season != "зима")}")
+    val condition = ((isSunny == IS_SUNNY) && (isAwningOpen == IS_AWNING_OPEN)
+            && (humidity == HUMIDITY) && (season == SEASON))
+
+    println("Благоприятные ли условия для роста бобовых? ${condition}")
 
 }
+
+const val HUMIDITY = 20
+const val IS_SUNNY: Boolean = true
+const val IS_AWNING_OPEN: Boolean = true
+const val SEASON = "не зима"
