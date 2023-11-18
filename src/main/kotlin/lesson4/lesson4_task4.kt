9987f8cbcb0cfdd3e1fb913cc48ba93e827ca405
Rodion <piperov2004@mail.ru>
2023-11-18 13:33:01 +0300

    KS-4-4 solution corrected

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -3,12 +3,13 @@ package lesson4
 fun main() {
 
     val numberOfDay = 5
+    val evenDay = (numberOfDay % 2 == 0)
 
     println("""
-        Упражнения для рук: ${numberOfDay %2 != 0}
-        Упражнения для ног: ${numberOfDay %2 == 0}
-        Упражнения для спины: ${numberOfDay %2 == 0}
-        Упражнения для пресса: ${numberOfDay %2 != 0}
+        Упражнения для рук: ${!evenDay}
+        Упражнения для ног: $evenDay
+        Упражнения для спины: $evenDay
+        Упражнения для пресса: ${!evenDay}
     """.trimIndent())
 
 }
