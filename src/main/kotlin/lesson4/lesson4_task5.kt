b33e1c0ff1a11aebfd85dc6f823df5bf07185508
Rodion <piperov2004@mail.ru>
2023-11-18 16:59:34 +0300

    KS-4-5 solution corrected

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -2,14 +2,14 @@ package lesson4
 
 fun main() {
 
-    println("Повреждения корабля: да/нет")
-    val damageOfShip: Boolean = (readln() == "да")
+    println("Повреждения корабля: true/false")
+    val damageOfShip: Boolean = readln().toBoolean()
     println("Число экипажа:")
     val numberOfCrew = readln().toInt()
     println("Количество ящиков с провизией на борту:")
     val numberOfProvisions = readln().toInt()
-    println("Благоприятность метеоусловий")
-    val weatherIsClear: Boolean = (readln() == "да")
+    println("Благоприятность метеоусловий: true/false")
+    val weatherIsClear: Boolean = readln().toBoolean()
 
     val mainNumberOfCrew = 55..70
     val mainNumberOfProvisions = 50
