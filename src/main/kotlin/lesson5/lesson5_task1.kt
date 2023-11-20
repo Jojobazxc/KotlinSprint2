11f8a73c3973b22f0ffc7bb4a63861150ad75fa1
Rodion <piperov2004@mail.ru>
2023-11-20 17:25:18 +0300

    KS-5-1 solution corrected

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,10 +1,15 @@
 package lesson5
 
+import kotlin.random.Random
+
 fun main() {
 
-    print("6 * 8 = ")
+    val firstNumber = Random.nextInt(1,50)
+    val secondNumber = Random.nextInt(1,50)
 
-    val mathExample: Boolean = ((6 * 8) == readln().toInt())
+    print("Для получение доступа решите пример: $firstNumber + $secondNumber = ")
+
+    val mathExample: Boolean = ((firstNumber + secondNumber) == readln().toInt())
 
     if (mathExample) println("Добро пожаловать!")
     else println("Доступ запрещен.")
