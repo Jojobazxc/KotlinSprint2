584e57a33f9644ba3061192f3c1f52d46432f523
Rodion <piperov2004@mail.ru>
2023-11-26 14:15:05 +0300

    KS-6-3 solution corrected

--- src/main/kotlin/lesson6/lesson6_task3.kt
+++ src/main/kotlin/lesson6/lesson6_task3.kt
@@ -5,16 +5,11 @@ fun main() {
     print("Введите колличество секунд: ")
     var userSeconds = readln().toInt()
 
-    val timer = userSeconds
-    var startTime = 0
-
-    while (timer != startTime) {
-        startTime++
+    while (userSeconds > 0) {
         println("Осталось секунд: ${userSeconds--}  ")
         Thread.sleep(1000)
     }
 
     println("Время вышло")
-    println("Прошло $timer секунд")
 
 }
