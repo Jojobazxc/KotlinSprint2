96c0868aef29c4182b36cea1d6accd24c2242110
Rodion <piperov2004@mail.ru>
2023-11-27 19:26:19 +0300

    KS-6-4 solution corrected 2

--- src/main/kotlin/lesson6/lesson6_task4.kt
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -1,23 +1,26 @@
 package lesson6
 
+import kotlin.math.abs
 import kotlin.random.Random
 
 fun main() {
 
     val randomNumber = Random.nextInt(1, 9)
     var userNumber: Int
+    var counterInStart: Int = 0
 
     println("Попробуй угадать число от 1 до 9")
-    while (COUNTER_OF_TRIES > 0) {
+    while (counterInStart < COUNTER_OF_TRIES) {
         userNumber = readln().toInt()
+        counterInStart++
         if (userNumber == randomNumber) {
             println("Это была великолепная игра!")
             return
-        } else println("Неверно, осталось попыток: ${--COUNTER_OF_TRIES}")
+        } else println("Неверно, осталось попыток: ${abs(counterInStart - COUNTER_OF_TRIES)} ")
     }
 
     println("Было загадано число $randomNumber")
 
 }
 
-var COUNTER_OF_TRIES = 5
+const val COUNTER_OF_TRIES = 5
