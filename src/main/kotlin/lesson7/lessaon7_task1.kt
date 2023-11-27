0499f00b3e3b6c54a7dfbeaf03cf0bad4a2727f1
Rodion <piperov2004@mail.ru>
2023-11-27 19:33:20 +0300

    KS-7-1 solution corrected

--- src/main/kotlin/lesson7/lessaon7_task1.kt
+++ src/main/kotlin/lesson7/lessaon7_task1.kt
@@ -1,18 +1,21 @@
 package lesson7
 
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 fun main() {
 
     var password = ""
     var char: String
+    val rangeOfSymbols = 'a'..'z'
+    val rangeOfNumbers = 1..9
 
     for (i in 0..<LENGHT_OF_PASSWORD) {
         if (i % 2 == 0) {
-            char = ('a'..'z').random().toString()
+            char = (rangeOfSymbols).random().toString()
             password += char
         } else {
-            char = Random.nextInt(1, 10).toString()
+            char = Random.nextInt(rangeOfNumbers).toString()
             password += char
         }
     }
