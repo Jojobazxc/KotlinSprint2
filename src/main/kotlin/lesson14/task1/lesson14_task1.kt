41bf589f6947a21d1b50968bc6156499a0ae2542
Rodion <piperov2004@mail.ru>
2023-12-04 18:03:30 +0300

    KS-14-1 solution

---
+++ src/main/kotlin/lesson14/task1/lesson14_task1.kt
@@ -0,0 +1,16 @@
+package lesson14.task1
+
+fun main() {
+
+    val liner1 = Liner()
+    liner1.printInformationOfShip()
+    println()
+
+    val icebreaker1 = Icebreaker()
+    icebreaker1.printInformationOfShip()
+    println()
+
+    val cargoShip = CargoShip()
+    cargoShip.printInformationOfShip()
+
+}
