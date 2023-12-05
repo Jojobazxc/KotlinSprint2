44e537d20af62d1d5837279e33316e92a1756b70
Rodion <piperov2004@mail.ru>
2023-12-05 19:23:55 +0300

    KS-16-1 solution

---
+++ src/main/kotlin/lesson16/task1/Cube.kt
@@ -0,0 +1,7 @@
+package lesson16.task1
+
+class Cube(private val randomNumber: Int) {
+    fun getRandomNumber() {
+        println("Число на кубе: $randomNumber")
+    }
+}
---
+++ src/main/kotlin/lesson16/task1/lesson16_task1.kt
@@ -0,0 +1,8 @@
+package lesson16.task1
+
+fun main() {
+
+    val cube1 = Cube((1..6).random())
+    cube1.getRandomNumber()
+
+}
