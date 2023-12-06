8b98acb59ed6c2699cfece9af42b59f0da4923f9
Rodion <piperov2004@mail.ru>
2023-12-06 14:38:36 +0300

    KS-11-3 solution

---
+++ src/main/kotlin/lesson11/task3/User1.kt
@@ -0,0 +1,27 @@
+package lesson11.task3
+
+class User1(
+    val avatar: String,
+    val nickname: String,
+    val statusOfSpeaking: String,
+)
+class Room(
+    val cover: String,
+    val nameOfRoom: String,
+    val userList: MutableList<User1>
+) {
+
+
+    fun getRoomInfo(selectRoom: Int, roomList: MutableList<Room>) {
+        println("Название комнаты: ${roomList[selectRoom].nameOfRoom}")
+        println("Обложка комнаты: ${roomList[selectRoom].cover}")
+        roomList[selectRoom].userList.forEach { user ->
+            println("Аватар: ${user.avatar} | Статус: ${user.statusOfSpeaking}")
+        }
+    }
+
+    fun getUserInfo(selectUser: Int) {
+        println(userList[selectUser].nickname)
+    }
+
+}
