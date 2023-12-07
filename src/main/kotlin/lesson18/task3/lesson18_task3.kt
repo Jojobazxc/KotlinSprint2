0bbb529e4d2f3569e4390047f7a010e6f2a8e5c4
Rodion <piperov2004@mail.ru>
2023-12-07 19:47:10 +0300

    KS-18-3 solution

---
+++ src/main/kotlin/lesson18/task3/lesson18_task3.kt
@@ -0,0 +1,20 @@
+package lesson18.task3
+
+fun main() {
+
+    val fox: Tamagotchi = Fox()
+    val dog: Tamagotchi = Dog()
+    val cat: Tamagotchi = Cat()
+    fox.getPlay()
+    fox.getEat()
+    fox.getSleep()
+    println()
+    dog.getPlay()
+    dog.getEat()
+    dog.getSleep()
+    println()
+    cat.getPlay()
+    cat.getEat()
+    cat.getSleep()
+
+}
