6965e00ea3ae5a1bb137eefc68b40e4a7c7d1963
Rodion <piperov2004@mail.ru>
2023-12-15 01:50:03 +0300

    KS-12-2 solution corrected

--- src/main/kotlin/lesson12/task2/lesson12_task2.kt
+++ src/main/kotlin/lesson12/task2/lesson12_task2.kt
@@ -6,3 +6,17 @@ fun main() {
     day.printInformation()
 
 }
+
+class Weather2(
+    private val dayTemperature: Int,
+    private val nightTemperature: Int,
+    private val isPrecipitationToday: Boolean
+) {
+
+    fun printInformation() {
+        println("Температура днем: $dayTemperature")
+        println("Температура ночью: $nightTemperature")
+        println("Осадки сегодня: $isPrecipitationToday")
+    }
+
+}
