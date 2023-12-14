a5f6ffc4e0e5257f82220ba293adc8d9b1d9ea80
Rodion <piperov2004@mail.ru>
2023-12-15 01:53:39 +0300

    KS-12-4 solution corrected

--- src/main/kotlin/lesson12/task4/lesson12_task4.kt
+++ src/main/kotlin/lesson12/task4/lesson12_task4.kt
@@ -5,3 +5,13 @@ fun main() {
     val day = Weather4(304, 299, false)
 
 }
+
+class Weather4(dayTemperature: Int, nightTemperature: Int, isPrecipitationToday: Boolean) {
+
+    init {
+        println("Температура днем: ${dayTemperature - 273}")
+        println("Температура ночью: ${nightTemperature - 273}")
+        println("Осадки сегодня: $isPrecipitationToday")
+    }
+
+}
