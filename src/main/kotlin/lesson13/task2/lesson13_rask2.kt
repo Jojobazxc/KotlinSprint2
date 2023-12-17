fb83375241f05f2ef908c08bd081479e09f087aa
Rodion <piperov2004@mail.ru>
2023-12-17 16:04:50 +0300

    KS-13-2 deleted class

--- src/main/kotlin/lesson13/task2/lesson13_rask2.kt
+++ src/main/kotlin/lesson13/task2/lesson13_rask2.kt
@@ -4,3 +4,12 @@ fun main() {
     val contact1 = Phonebook2("Александр", 88005553535, null)
     contact1.printInfo()
 }
+
+class Phonebook2(private val name: String, private val phoneNumber: Long, private val company: String? = null) {
+    fun printInfo() {
+        println(
+            "-Имя: $name \n-Номер телефона: $phoneNumber \n-Компания: ${company ?: "не указано"}"
+        )
+
+    }
+}
