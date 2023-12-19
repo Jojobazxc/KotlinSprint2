1a6904b8c96ba55216782235487214b702e7602f
Rodion <piperov2004@mail.ru>
2023-12-19 18:14:50 +0300

    KS-14-4 solution corrected

--- src/main/kotlin/lesson14/task4/lesson4_task4.kt
+++ src/main/kotlin/lesson14/task4/lesson4_task4.kt
@@ -8,3 +8,20 @@ fun main() {
     val mars = Planet("Марс", listOf(phobos, deimos))
     mars.printInfo()
 }
+
+open class CelestialBody(
+    val name: String,
+    val hasAtmosphere: Boolean = false,
+    val suitableForLanding: Boolean = false
+) {
+}
+
+class Planet(name: String, private val satellites: List<Satellite> = emptyList()) : CelestialBody(name) {
+    fun printInfo() {
+        println("Планета: $name")
+        satellites.forEach { println("\tСпутник: ${it.name}") }
+    }
+}
+
+
+class Satellite(name: String) : CelestialBody(name)
