cee3189ae28218859ff559a9626c17bc10806b22
Rodion <piperov2004@mail.ru>
2023-12-19 19:33:10 +0300

    KS-19-4 deleted class

--- src/main/kotlin/lesson19/task4/lesson19_task4.kt
+++ src/main/kotlin/lesson19/task4/lesson19_task4.kt
@@ -13,9 +13,22 @@ fun main() {
 
 }
 
-
-enum class Patrons(val damage: Int, name: String){
+enum class Patrons(val damage: Int, name: String) {
     BLUE(5, "Cиние"),
     GREEN(10, "Зеленые"),
     RED(20, "Красные"),
+}
+
+class Tank(private var typeOfPatrons: Patrons?) {
+
+    fun armedANewPatrons(patrons: Patrons) {
+        typeOfPatrons = patrons
+        println("Перезарядка патронами вида: ${patrons.name}")
+    }
+
+    fun takeAShot(patrons: Patrons) {
+        println("Произведен выстрел патронами вида: ${patrons.name}. Нанесен урон: ${patrons.damage} едениц")
+    }
+
+
 }
