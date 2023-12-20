f3773a781f2c1912805fa1f17f551279247dd362
Rodion <piperov2004@mail.ru>
2023-12-20 15:12:17 +0300

    KS-15-2 deleted class

--- src/main/kotlin/lesson15/task2/lesson15_task2.kt
+++ src/main/kotlin/lesson15/task2/lesson15_task2.kt
@@ -11,3 +11,31 @@ fun main() {
     weatherServer.sendDataToServer(precipitationAmount)
 
 }
+
+abstract class WeatherStationStats() {
+    abstract val value: Int
+}
+
+class Temperature(
+    override val value: Int
+) : WeatherStationStats()
+
+class PrecipitationAmount(
+    override val value: Int
+) : WeatherStationStats()
+
+class WeatherServer {
+    fun sendDataToServer(data: WeatherStationStats) {
+        when (data) {
+            is Temperature -> {
+                println("Сервер: температура - ${data.value}")
+            }
+
+            is PrecipitationAmount -> {
+                println("Сервер: кол-во осадков - ${data.value}")
+            }
+
+            else -> println("Неверный тип данных")
+        }
+    }
+}
