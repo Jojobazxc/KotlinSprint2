a555b8d70b5939c8e4f33cd73606f7aa47e01fa4
Rodion <piperov2004@mail.ru>
2023-12-21 15:45:54 +0300

    KS-16-2 deleted class-file

--- src/main/kotlin/lesson16/task2/lesson16_task2.kt
+++ src/main/kotlin/lesson16/task2/lesson16_task2.kt
@@ -8,3 +8,19 @@ fun main() {
     circle.getAreaOfCircle()
 
 }
+
+class Circle(private val radius: Float) {
+
+    private val pi: Float = 3.14F
+
+    fun getAreaOfCircle() {
+        val areaOfCircle = pi * radius * radius
+        println("Площадь круга: ${"%.2f".format(areaOfCircle)}")
+    }
+
+    fun getLengthOfCircle() {
+        val lengthOfCircle = 2 * pi * radius
+        println("Длина круга: ${"%.2f".format(lengthOfCircle)}")
+    }
+
+}
