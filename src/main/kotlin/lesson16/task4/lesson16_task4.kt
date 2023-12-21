37ecfe98ffdce5d8945ec3a8524a42b7bfbfa30c
Rodion <piperov2004@mail.ru>
2023-12-21 15:49:52 +0300

    KS-16-4 deleted class-files

--- src/main/kotlin/lesson16/task4/lesson16_task4.kt
+++ src/main/kotlin/lesson16/task4/lesson16_task4.kt
@@ -13,3 +13,24 @@ fun main() {
     } else return
 
 }
+
+class Order {
+    private var statusOfOrder = "не готов"
+    private val numberOfOrder = 11111
+
+    fun getAnswerFromManager(): Boolean {
+        println("Ответ менеджера: ")
+        return readln().toBoolean()
+    }
+
+    fun changeStatusOfOrder(answerOfManager: Boolean, newStatus: String) {
+        if (answerOfManager) {
+            statusOfOrder = newStatus
+            println("Статус заказа успешно изменен")
+        } else println("Менеджер не одобрил заявку")
+    }
+
+    fun printCurrentStatusOfOrder() {
+        println("Текущий статус заказа: $statusOfOrder")
+    }
+}
--- src/main/kotlin/lesson16/task4/lesson16_task4_second.kt
+++ src/main/kotlin/lesson16/task4/lesson16_task4_second.kt
@@ -1,5 +1,27 @@
 package lesson16.task4
 
+import java.util.*
+
 fun main() {
     Order2().changeReadyStatus()
+}
+
+class Order2(var readyStatus: String = "Заказ не подтверждён") {
+    private val numberOfOrder = "39847164467"
+
+    fun changeReadyStatus() {
+        if (sendMassage()) {
+            readyStatus = "Заказ подтверждён"
+            println("Статуз заказа: $readyStatus")
+        } else {
+            readyStatus = "Заказ ожидает подтверждения"
+            println("Статуз заказа: $readyStatus")
+        }
+    }
+
+}
+
+fun sendMassage(): Boolean {
+    println("Подвердить заказ у менеджера? да/нет")
+    return readln().lowercase(Locale.getDefault()) == "да"
 }
