17c3200639243a36688a3414394088682878188d
Rodion <piperov2004@mail.ru>
2023-12-25 20:44:21 +0300

    KS-17-2 deleted class-file

--- src/main/kotlin/lesson17/task2/lesson17_task2.kt
+++ src/main/kotlin/lesson17/task2/lesson17_task2.kt
@@ -4,3 +4,13 @@ fun main() {
     val ship1 = Ship()
     ship1.name = "Ship2"
 }
+
+class Ship {
+    var name: String = "Ship"
+        set(value) {
+            field = "Ship"
+            println("Имя $field нельзя перезаписать")
+        }
+    var speed: Int = 50
+    var port: String = "Port1"
+}
