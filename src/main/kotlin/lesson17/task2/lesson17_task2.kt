229842a705bf9450a60b8c208cf48ceb2681ddda
Rodion <piperov2004@mail.ru>
2024-01-03 13:51:21 +0300

    KS-17-2 solution corrected

--- src/main/kotlin/lesson17/task2/lesson17_task2.kt
+++ src/main/kotlin/lesson17/task2/lesson17_task2.kt
@@ -7,10 +7,7 @@ fun main() {
 
 class Ship {
     var name: String = "Ship"
-        set(value) {
-            field = "Ship"
-            println("Имя $field нельзя перезаписать")
-        }
+        set(value) = println("Имя $field нельзя перезаписать")
     var speed: Int = 50
     var port: String = "Port1"
 }
