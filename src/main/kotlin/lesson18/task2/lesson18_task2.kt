28fec1763424e0024f5ba2696fa70d8d1b2072da
Rodion <piperov2004@mail.ru>
2024-01-05 18:30:55 +0300

    KS-18-2 deleted class-files

--- src/main/kotlin/lesson18/task2/lesson18_task2.kt
+++ src/main/kotlin/lesson18/task2/lesson18_task2.kt
@@ -10,3 +10,33 @@ fun main() {
     eightEdgesDice.getRandomNumber()
 
 }
+
+open class Dice {
+
+    open fun getRandomNumber() {}
+
+}
+
+class FourEdges(
+    private val quantityOfEdges: Int
+) : Dice() {
+    override fun getRandomNumber() {
+        println("Выпавшее число: ${(1..quantityOfEdges).random()}")
+    }
+}
+
+class SixEdges(
+    private val quantityOfEdges: Int
+) : Dice() {
+    override fun getRandomNumber() {
+        println("Выпавшее число: ${(1..quantityOfEdges).random()}")
+    }
+}
+
+class EightEdges(
+    private val quantityOfEdges: Int
+) : Dice() {
+    override fun getRandomNumber() {
+        println("Выпавшее число: ${(1..quantityOfEdges).random()}")
+    }
+}
