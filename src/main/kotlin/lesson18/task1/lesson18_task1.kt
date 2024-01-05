79a3c482b7f35416261b01b1daf170ed476bcbd6
Rodion <piperov2004@mail.ru>
2024-01-05 18:29:57 +0300

    KS-18-1 solution deleted class-files

--- src/main/kotlin/lesson18/task1/lesson18_task1.kt
+++ src/main/kotlin/lesson18/task1/lesson18_task1.kt
@@ -14,3 +14,21 @@ fun main() {
     )
 
 }
+
+class Order {
+
+    fun getInfoAboutOrder(
+        numberOfOrder: Int,
+        product: String,
+    ) {
+        println("Заказан товар: $product")
+    }
+
+    fun getInfoAboutOrder(
+        numberOfOrder: Int,
+        products: List<String>,
+    ) {
+        println("Заказаны следующие товары: ${products.joinToString(", ")}")
+    }
+
+}
